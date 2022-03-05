8dd8baac9c1fe78533cbdf56558db5f082ccdb80
miglezub <miglezub@gmail.com>
2022-03-05 12:57:37 +0200

    create admin layout

---
+++ resources/views/admin/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+    <h2>
+        <label for="nav-toggle">
+            <span class="fas fa-bars"></span>
+        </label>
+
+        Pagrindinis
+    </h2>
+    <div class="search-wrapper">
+        <span class="fas fa-search"></span>
+        <input type="search" placeholder="Įveskite raktažodį" />
+    </div>
+
+    <div class="user-wrapper">
+        <img src="admin/assets/images/image.jpg" width="35px" height="40px" alt="">
+        <div>
+            <h4> Agne Jonaitiene</h4>
+            <small>Administratorius</small>
+
+        </div>
+    </div>
+</header>
---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,43 @@
+<div class="sidebar">
+    <div class="sidebar-brand">
+        <h2><span class="fas fa-tooth"></span> <span>DentalClinic</span></h2>
+    </div>
+    <div class="sidebar-menu">
+        <ul>
+            <li>
+                <a href="admin/html/index.html" class="active"><span class="fas fa-home"></span>
+                    <span>Pagrindinis</span></a>
+            </li>
+
+            <li>
+                <a href="admin/html/patients.html"><span class="fas fa-users"></span>
+                    <span>Pacientai</span></a>
+            </li>
+
+            <li>
+                <a href="admin/html/doctors.html"><span class="fas fa-user-md"></span>
+                    <span>Daktarai</span></a>
+            </li>
+
+            <li>
+                <a href="admin/html/appointments.html"><span class="fas fa-calendar-check"></span>
+                    <span>Vizitai</span></a>
+            </li>
+
+            <li>
+                <a href="admin/htmlschedule.html"><span class="fas fa-calendar-alt"></span>
+                    <span>Tvarkaraštis</span></a>
+            </li>
+
+            <li>
+                <a href="admin/html/procedures.html"><span class="fas fa-hand-holding-medical"></span>
+                    <span>Procedūros</span></a>
+            </li>
+
+            <li>
+                <a href="admin/html/currentuser.html"><span class="fas fa-user-cog"></span>
+                    <span>Paskyra</span></a>
+            </li>
+        </ul>
+    </div>
+</div>
