bbdaf8c331b3dc23e0542b819782537c965346ef
miglezub <miglezub@gmail.com>
2022-03-05 14:36:15 +0200

    add admin routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProcedureController;
+use App\Http\Controllers\Admin\ProcedureController;
 use App\Http\Controllers\HomeController;
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Models\Procedure;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,9 +27,12 @@ Route::post('post-register', [AuthController::class, 'postRegister'])->name('reg
 
 Route::get('/', [HomeController::class, 'index']);
 
+Route::group(['middleware' => 'is.admin', 'namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::get('', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin');
+    Route::get('/procedure', [ProcedureController::class, 'index'])->name('admin.procedure');
+});
 
-
-Route::get('/home', [HomeController::class, 'redirect']);
+// Route::get('/home', [HomeController::class, 'redirect']);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
