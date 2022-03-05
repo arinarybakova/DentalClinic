dc1910db16bb2b07f329385fb8093f6a95003ab5
miglezub <miglezub@gmail.com>
2022-03-05 14:34:33 +0200

    add admin procedure controller

---
+++ app/Http/Controllers/Admin/ProcedureController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Procedure;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
+class ProcedureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    //    $procedures = Procedure::all();
+    //    return response()->json($procedures);
+    return view("admin.procedure");
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $procedure = Procedure::create($request->post());
+        return response()->json([
+            'procedure'=>$procedure
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Procedure $procedure)
+    {
+        return response()->json($procedure);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Procedure $procedure)
+    {
+        $procedure->fill($request->post())->save();
+        return response()->json([
+            'procedure'=>$procedure
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Procedure $procedure)
+    {
+        $procedure->delete();
+        return response()->json([
+            'message' => 'Procedure removed'
+        ]);
+    }
+}
