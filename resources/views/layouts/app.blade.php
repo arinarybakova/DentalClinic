bd8062b1bcb641491f2e0604ef60bedf5b106b05
miglezub <miglezub@gmail.com>
2022-03-05 12:54:27 +0200

    create user layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,46 +1,63 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <!--font awesome cdn link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-        @livewireStyles
+    <!--custom css file link-->
+    <link rel="stylesheet" href="{{ mix('css/user.css') }}">
+    <a href="https://storyset.com/people">People illustrations by Storyset</a>
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-        <div class="">
-            @livewire('navigation-menu')
+    @livewireStyles
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+
+<body>
+    <!--header section starts-->
+    <header class="header">
+        <a href="#" class="logo"> <i class="fas fa-tooth"></i> dentalcare. </a>
+        <nav class="navbar">
+            <a href="#home">Pagrindinis</a>
+            <a href="#services">Paslaugos</a>
+            <a href="#about">Apie mus</a>
+            <a href="#doctors">Specialistai</a>
+            @if(Route:: has('login'))
+
+            @auth
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+
+                <a href="{{route('login')}}">Atsijungti</a>
+            </form>
+            @else
+            <a href="{{route('login')}}">Prisijungti</a>
+            <a href="{{route('register')}}">Registruotis</a>
+
+            @endauth
+
             @endif
+        </nav>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+        <!--menu mygtukas kai ekrano dydis mazesnis-->
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </header>
+    <!--header section ends-->
+    <div class="container mt-5" id="app">
+        @yield('content')
+    </div>
 
-        @stack('modals')
-
-        @livewireScripts
-    </body>
+    <!-- footer section ends -->
+</body>
 </html>
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,54 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+@extends('layouts.app')
 
-    <!--font awesome cdn link-->
-    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+@section('title', 'Home Page')
 
-    <!--custom css file link-->
-    <link rel = "stylesheet" href = "../assets/css/styles.css">
-    <a href="https://storyset.com/people">People illustrations by Storyset</a>
-
-</head>
-<body>
-   <!--header section starts--> 
-   <header class="header">
-        <a href="#" class="logo"> <i class="fas fa-tooth"></i> dentalcare. </a>
-        <nav class="navbar">
-            <a href="#home">Pagrindinis</a>
-            <a href="#services">Paslaugos</a>
-            <a href="#about">Apie mus</a>
-            <a href="#doctors">Specialistai</a>
-            @if(Route:: has('login'))
-
-            @auth
-            <x-app-layout>
-            </x-app-layout>
-
-            @else
-            <a href="{{route('login')}}">Prisijungti</a>
-            <a href="{{route('register')}}">Registruotis</a>
-
-            @endauth
-
-            @endif
-        </nav>
-
-        <!--menu mygtukas kai ekrano dydis mazesnis-->
-      <div id="menu-btn" class="fas fa-bars"></div>  
-   </header>
-    <!--header section ends--> 
-
-
-
-<!-- footer section ends -->
-
-
-<!-- custom js file link-->
-<script src = "../assets/js/script.js"></script>
-</body>
-</html>
+@section('content')
+    Test homepage layout
+    <example-component></example-component>
+@endsection
