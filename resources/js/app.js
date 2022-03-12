a705129b89952f74ad4623c75752b32aec649f77
miglezub <miglezub@gmail.com>
2022-03-12 19:49:11 +0200

    initialise vue sweetalert

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,6 +17,11 @@ import axios from 'axios';
 import Vue from 'vue'
 import { BootstrapVue } from 'bootstrap-vue'
 
+import VueSweetalert2 from 'vue-sweetalert2';
+// If you don't need the styles, do not connect
+import 'sweetalert2/dist/sweetalert2.min.css';
+Vue.use(VueSweetalert2);
+
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
