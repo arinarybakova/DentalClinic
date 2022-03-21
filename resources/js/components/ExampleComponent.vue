497fe0da71d416503300cc915ef5ad2deaf2fb0f
arinarybakova <ar.rybakova@gmail.com>
2022-03-21 12:30:11 +0200

    login ui change

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,17 +1,24 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
+<section class="services" id="services">
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
+    <h1 class="headings">teikiamos <span>procedūros</span></h1>
+
+    <div class ="box-container">
+
+        <div class ="box">
+            <h3>Kanalo plombavimas</h3>
+            <p>
+                plombavimas, atliekamas naudojant dentalinį „Carl Zeiss“ OPMI pico markės mikroskopą. 
+                Pasitelkiant mikroskopą galima pamatyti tai, ko neįmanoma pamatyti plika akimi: siauras kanalų įeigas, 
+                papildomus kanalus, dantų šaknų skilimus ar traumos pasėkoje kalcifikuotus kanalus.
+            </p>
+            <div class = "cost">Kaina: 20 eurų</div>
+            <!--<a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span></a>-->
+        </div>    
     </div>
+   
+</section>
+    
 </template>
 
 <script>
@@ -21,3 +28,64 @@
         }
     }
 </script>
+
+<style>
+.headings{
+    text-align: center;
+    padding-bottom: 2rem;
+    text-transform: uppercase;
+    color: var(--black);
+    font-size: 5rem;
+    padding-top: 10rem;
+    letter-spacing: .4rem;
+}
+.headings span{
+    text-transform: uppercase;
+    color: var(--green);
+    font-size: 5rem;
+    letter-spacing: .4rem;
+}
+
+.services .box-container{
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(43rem, 1fr));
+    gap: 2rem;
+}
+
+.services .box-container .box{
+    background: #fff;
+    border-radius:  .5rem;
+    box-shadow: var(--box-shadow);
+    border: var(--border);
+    padding: 2.5rem;
+}
+
+
+.services .box-container .box i{
+    color: var(--green);
+    font-size: 5rem;
+    padding-bottom: .5rem;
+}
+
+.services .box-container .box h3{
+    color: var(--black);
+    font-size: 2.5rem;
+    padding: 1rem 0;
+    text-transform: none;
+    color: var(--green);
+}
+
+.services .box-container .box p{
+    color: var(--light-color);
+    font-size: 1.4rem;
+    line-height: 2;
+    text-transform: none;
+}
+.services .cost{
+    color: var(--green);
+    text-transform: none;
+    font-size: 1.5rem;
+    font-weight: bold;
+    text-align: right;
+}
+</style>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!--font awesome cdn link-->
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+        <div>
+    </div>
+        <div class = "txt-lg"><i id = icon class="fas fa-tooth"></i>Odontologijos klinika</div>
+        <div class = "txt-lb"> Prisijungimas </div>
+            <!--<x-jet-authentication-card-logo />-->
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
@@ -14,35 +24,114 @@
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
-
             <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
+               <x-jet-label for="email" value="{{ __('El. paštas') }}" /></div>
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+            </id>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-label for="password" value="{{ __('Slaptažodis') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
-            <div class="block mt-4">
+            <!--<div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
+            </div>-->
+            <x-jet-button class="ml-4">
+                    {{ __('Prisijungti') }}
                 </x-jet-button>
-            </div>
+            <div class="flex items-center justify-end mt-4">
+                
+                @if (Route::has('password.request'))
+               
+                <div class = "lb-psw"> Pamiršote slaptažodį? <a class="text-smi" href="{{ route('password.request') }}">
+                    {{ __('Priminti') }}
+                    </a></div>
+                @endif
+                </div>
+                <div class = "lb-psws"> Neturite paskyros? <a class="text-smi" href="{{ route('register') }}">
+                {{ __('Registruotis') }}
+                </a></div>
+
+               
+            
         </form>
     </x-jet-authentication-card>
 </x-guest-layout>
+
+<style>
+    #icon {
+        color: #16a085;
+        margin-top: 1rem;   
+    }
+    .txt-lg{
+        text-align: center;
+        padding-top: 5px;
+        padding-bottom: 80px;
+        color: #16a085;
+        font-size: 3.2rem;
+        
+    }
+    .txt-lb{
+        text-align: center;
+        padding-top: 10px;
+        font-size: 1.9rem;
+        color: #444;
+        font-weight: bold;
+        
+    }
+    .ml-4{
+       background-color: #16a085;
+       border-radius: 12px;
+       font-size: 1rem;
+       margin-top: 30px;
+       margin-left: 30%;
+       margin-right: 30%
+    }
+    .ml-4:hover{
+       background-color: #444;
+    }
+    .lb-psw{
+        display: inline-block;  
+        padding-bottom: 1rem;
+        white-space: nowrap;
+        padding: 15px 137px 0px 1px;
+        color: #444;
+        font-size: 1.12rem;
+        font-weight: bold;
+        float: left;    
+    }
+    .lb-psws{
+        display: inline-block;  
+        padding-bottom: 1rem;
+        white-space: nowrap;
+        padding: 5px 72px 0px 1px;
+        color: #444;
+        font-size: 1.12rem;
+        font-weight: bold;      
+    }
+    .text-sm{
+        text-decoration:none;
+        color: #444;
+        font-size: 1.1rem;
+    }
+    .text-smi{
+        text-decoration:none;
+        color: #16a085;
+        font-size: 1.12rem;
+        font-weight: bold;
+        margin-left: 0.5rem;   
+        
+    }
+    .text-smi:hover{
+        text-decoration:none;
+        color: #444;
+    }
+    .label{
+        font-size: 5rem;
+        color: #444;
+    }
+</style>
