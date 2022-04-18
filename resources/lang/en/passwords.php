6a8abed9a744ed8c99e4dcf920e24a84799d1a59
arinarybakova <ar.rybakova@gmail.com>
2022-04-18 18:48:17 +0300

    ui changes progress save

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
+    'reset' => 'Jūsų slaptažodis nustatytas iš naujo!',
+    'sent' => 'Slaptažodžio atstatymo nuoroda išsiųsta į nurodytą el. paštą',
     'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'token' => 'Neteisingas slaptažodio atstatymo raktas',
+    'user' => "Įvestas el. paštas nėra užregistruotas sistemoje",
 
 ];
